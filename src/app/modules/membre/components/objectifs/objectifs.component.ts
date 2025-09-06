6c584f9b2f336afb31afd00c8f3252fc417c6dd2
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2025-09-06 14:37:44 +0100

    Update objectifs.component.ts

--- src/app/modules/membre/components/objectifs/objectifs.component.ts
+++ src/app/modules/membre/components/objectifs/objectifs.component.ts
@@ -46,7 +46,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatDialogModule,
     MatDatepickerModule,
     MatExpansionModule, // Ajouté,
-    MatProgressBarModule
+    MatProgressBarModule,
     MatListModule],
   templateUrl: './objectifs.component.html',
   styleUrl: './objectifs.component.scss'
