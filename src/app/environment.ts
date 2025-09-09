c56d63444c3c851acf75ff551f7818072d1ff2cf
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2025-09-09 19:00:54 +0000

    Update environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: false,
-<<<<<<< HEAD
-  apiUrl: 'http://localhost:8080/api',
-  //apiUrl:'https://mean-fredelia-mon2-0-279bc751.koyeb.app/api'
-=======
   //apiUrl: 'http://localhost:8080/api',
   apiUrl:'https://mean-fredelia-mon2-0-279bc751.koyeb.app/api'
->>>>>>> 8cbf74c465de4322df1c820fa51c94bb678e3e0b
 
 };
