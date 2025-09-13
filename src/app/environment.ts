0426f214c7ecb30aed53102e142110176d383d78
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2025-09-13 08:11:57 +0000

    Update environment.ts

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api',
-  apiUrl:'https://high-wandis-mon2-0-d3adfcdd.koyeb.app/api'
+  apiUrl:'https://lexical-sybil-mon2-0-76700d64.koyeb.app/api'
 
 };
