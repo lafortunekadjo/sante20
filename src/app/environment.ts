14be09de8a42a2feb8426a6935c9c53b4a28c25f
vkadjo <lafortunekadjo@outlook.fr>
2025-09-21 10:16:30 +0000

    environment

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   //apiUrl: 'http://localhost:8080/api',
-  apiUrl:'https://mean-fredelia-mon2-0-279bc751.koyeb.app/api'
+  apiUrl:'https://lexical-sybil-mon2-0-76700d64.koyeb.app/api'
 
 };
