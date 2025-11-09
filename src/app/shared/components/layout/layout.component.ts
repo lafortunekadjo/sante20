47aebc87178ab90a79851b640e873fd68a540aa1
lafortunekadjo <79406164+lafortunekadjo@users.noreply.github.com>
2025-11-09 01:04:49 +0100

    Update layout.component.ts

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -268,7 +268,7 @@ export class LayoutComponent implements OnInit, OnDestroy {
    * Charger les menus de l'utilisateur pour le groupe actuel (si responsable)
    */
   loadUserMenus(): void {
-   console.log("lodmenu", !this.isResponsable)
+   
     // La vérification isLoggedIn() est faite par l'abonnement à isUserReady$
     
     if (!this.isResponsable) {
@@ -277,7 +277,7 @@ export class LayoutComponent implements OnInit, OnDestroy {
     }
 
     const groupeId = this.authService.getCurrentGroupeId();
-    
+    console.log("lodmenu", !this.isResponsable, groupeId)
     if (!groupeId) {
       this.menuCategories = [];
       return;
