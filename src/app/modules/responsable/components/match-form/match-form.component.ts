b653d4210d1534a86b5a0599a77f818d3282c6cc
vkadjo <lafortunekadjo@outlook.fr>
2026-04-17 17:34:13 +0100

    generation des matchs

--- src/app/modules/responsable/components/match-form/match-form.component.ts
+++ src/app/modules/responsable/components/match-form/match-form.component.ts
@@ -1089,19 +1089,19 @@ export class MatchFormComponent implements OnInit, AfterViewInit, OnDestroy {
   generateMatchDatesInRange(dayOfWeek: string, startDate: Date, endDate: Date): Date[] {
     const dates: Date[] = [];
     let currentDate = new Date(startDate);
-   
+    
     // Mapping des jours (français et anglais)
     const daysMapping: { [key: string]: number } = {
       // Français
-      'Dimanche': 0, 'Lundi': 1, 'Mardi': 2, 'Mercredi': 3, 
-      'Jeudi': 4, 'Vendredi': 5, 'Samedi': 6,
+      'Dimanche': 2, 'Lundi': 2, 'Mardi': 3, 'Mercredi': 4, 
+      'Jeudi': 5, 'Vendredi': 6, 'Samedi': 7,
       // Anglais
-      'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday':3, 
-      'Thursday': 4, 'Friday': 5, 'Saturday': 6
+      'Sunday': 1, 'Monday': 2, 'Tuesday': 3, 'Wednesday': 4, 
+      'Thursday': 5, 'Friday': 6, 'Saturday': 7
     };
     
     const targetDayIndex = daysMapping[dayOfWeek] ?? 0; // Dimanche par défaut
-    console.log(targetDayIndex)
+
     while (currentDate <= endDate) {
       if (currentDate.getDay() === targetDayIndex) {
         dates.push(new Date(currentDate));
