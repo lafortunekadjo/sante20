0dd96b99e6b7ed7f5ed6aaa4a36e7d444317a265
vkadjo <lafortunekadjo@outlook.fr>
2026-06-03 11:12:10 +0100

    ajustement checkin

--- src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,10 +1,11 @@
 import { Component, Inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [MatDialogModule, MatButtonModule, TranslateModule],
   templateUrl: './confirmation-dialog.component.html',
   styleUrl: './confirmation-dialog.component.scss'
 })
